5f643371a8c307e15d0aa0b41a44f1a104dfb8bf
rajarshi30 <45178851+rajarshi30@users.noreply.github.com>
2018-11-19 18:29:15 +0000

    Add files via upload

---
+++ Jnkinsdemo.java
@@ -0,0 +1,13 @@
+package jenkindemo;
+
+import org.testng.annotations.Test;
+import org.testng.TestNG;
+
+public class Jnkinsdemo {
+
+	@Test
+	public void testjenkin(){
+		System.out.println("welcome to jenkins");
+	}
+
+}
